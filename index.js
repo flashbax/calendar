f77ac2a8d91082c54fc7b37fc2ce5b54f452087a
flashbax <jkiwtco@gmail.com>
2017-05-12 12:20:26 -0400

    Commit 2

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 window.onload = function() {
-    
 
-    //make the divs
+
+    //make the div for days
     function makeDays() { 
+    //var numOfDays = month;
+    var monthName = "January" 
+
+    var divMonth = document.createElement('div');
+    divMonth.setAttribute('id', 'month');
+    divMonth.innerHTML = monthName; 
+    document.getElementById('body').appendChild(divMonth);
+
+
         //loop through and create 9 divs with ids
         for (var i = 0; i<=30; i++) {  
             var divDay = document.createElement('div');     
@@ -13,6 +22,13 @@ window.onload = function() {
             divDay.style.border = "1px solid #000000";
             //text in div (date)
             divDay.innerHTML = i+1;
+            divDay.style.float = "left";
+            
+
+            if (i % 7 === 0) {
+                divDay.style.float = "left"; 
+                divDay.style.clear = "both";
+            };
 
             //add divs as child of body tag
             document.getElementById('body').appendChild(divDay);
